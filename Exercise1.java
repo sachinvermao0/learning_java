1b2575937672e48947c5ffc5f0c51e02f18f1ed9
alokvermaotw <alokvermaotw@gmail.com>
2026-07-14 00:09:34 +0530

    Solution 1: Print hello and your name in seperate line

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+// Java 1: Print hello and your name on a separate lines
+
+
+public class Exercise1 { 
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello\nSachin Kumar!");
+    } 
+}
+
