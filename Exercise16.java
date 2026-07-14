cde1292d97e58ec747db9082dd6491b7de17aa14
alokvermaotw <alokvermaotw@gmail.com>
2026-07-15 00:02:49 +0530

    Print a Face

---
+++ Exercise16.java
@@ -0,0 +1,22 @@
+/*16. Face Printer
+
+Write a Java program to print a face.
+Expected Output
+
+ +"""""+                                                 
+[| o o |]                                                
+ |  ^  |                                                 
+ | '-' |                                                 
+ +-----+
+*/
+
+public class Exercise16 {
+    public static void main(String[] arges) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+    }
+    
+}
