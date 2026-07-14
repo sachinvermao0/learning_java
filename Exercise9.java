0145b8cbdd414efbf289c96f8157beffc1b6576d
alokvermaotw <alokvermaotw@gmail.com>
2026-07-14 13:34:28 +0530

    Solution

---
+++ Exercise9.java
@@ -0,0 +1,17 @@
+/*9. Expression Evaluation
+
+Write a Java program to compute the specified expressions and print the output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889
+*/
+public class Exercise9 {
+
+    public static void main(String[] args) {
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+
+}
+    
+
