2670af2b36c664ad4ba3f5c1f802c1c4f08044e1
alokvermaotw <alokvermaotw@gmail.com>
2026-07-14 16:25:54 +0530

    Solution

---
+++ Exercise14a.java
@@ -0,0 +1,14 @@
+public class Exercise14a {
+    public static void main(String[] args) {
+    String p1 = "* * * * * * ==================================\n * * * * *  ==================================";
+    String p2 = "==============================================";
+    for (int i = 0; i < 4; i++) {
+            System.out.println(p1);
+    System.out.println("* * * * * * ==================================");
+    for (int j = 0; j < 6; j++) {
+            System.out.println(p2);
+        }
+    }
+
+    }
+}
