0efcda76117ff3e2d5c92a97d749036852679139
alokvermaotw <alokvermaotw@gmail.com>
2026-07-15 17:48:52 +0530

    Compare two numbers

---
+++ Exercise32.java
@@ -0,0 +1,32 @@
+//Write a  Java program to compare two numbers
+
+import java.util.Scanner;
+
+public class Exercise32 {
+   public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    
+    int number1;
+    int number2;
+
+    System.out.print("Input first integer: ");
+    number1 = input.nextInt();
+
+    System.out.print("Input second integer: ");
+    number2 = input.nextInt();
+
+    if (number1 == number2)
+        System.out.printf("%d == %d\n", number1, number2);
+    if (number1 != number2)
+        System.out.printf("%d != %d\n", number1, number2);
+    if (number1 < number2)
+        System.out.printf("%d < %d\n", number1, number2);
+    if (number1 > number2)
+        System.out.printf("%d > %d\n", number1, number2);
+    if (number1 <= number2)
+        System.out.printf("%d <= %d\n", number1, number2);
+    if (number1 >= number2)
+        System.out.printf("%d >= %d\n", number1, number2);
+   } 
+}
