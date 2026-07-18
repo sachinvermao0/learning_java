debcfc0cbb64e450bdfedc2d0d43ed743017729d
Sachin <919sachinkumar@gmail.com>
2026-07-19 00:54:22 +0530

    leap year or not

--- Conditional8.java
+++ Conditional8.java
@@ -1,3 +1,7 @@
+//Write a  Java program that requires the user to enter a single character from the alphabet.
+//  Print Vowel or Consonant, depending on user  input. If the user input is not 
+// a letter (between a and z or A and Z), or is a string of length > 1, print an error message.
+
 import java.util.Scanner;
 
 public class Conditional8 {
