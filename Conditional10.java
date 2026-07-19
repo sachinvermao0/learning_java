9d84adf0c3920532309d04ff4ed37cd1fecd4758
Sachin <919sachinkumar@gmail.com>
2026-07-19 12:53:27 +0530

    first 10 natural numbers

---
+++ Conditional10.java
@@ -0,0 +1,15 @@
+// Write a Java program to display the first 10 natural numbers
+
+public class Conditional10 {
+   public static void main(String[] args) {
+
+        int i;
+        
+        System.out.println("The first 10 natural numbers are:\n");
+
+        for (i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    System.out.println("\n");
+   } 
+}
