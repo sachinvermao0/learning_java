c36dddd9ffef98784e70b71960171f495cee0f60
Sachin <919sachinkumar@gmail.com>
2026-07-20 00:11:36 +0530

    angle triangle with a number

---
+++ Conditional13.java
@@ -0,0 +1,22 @@
+// Write a  Java program to display the pattern like a right angle triangle with a number.
+
+import java.util.Scanner;
+public class Conditional13 {
+    public static void main(String[] args) {
+
+
+        int i,j,n;
+        System.out.print("Input numbers of Row : ");
+      Scanner in = new Scanner(System.in);
+              n = in.nextInt();
+
+
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= n; j++)
+                System.out.println(j);
+
+            System.out.println("");
+        }
+}
+}
