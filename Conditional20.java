77871f8ceb7a5419a0cffa8b679a63ebbc351dc5
Sachin <919sachinkumar@gmail.com>
2026-07-21 12:12:38 +0530

    floatingpoint number checks

---
+++ Conditional20.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+public class Conditional20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first floating­point number: ");
+        double num1 = in.nextDouble();
+        System.out.print("Input second floating­point number");
+        double num2 = in.nextDouble();
+        in.close();
+
+
+        if (Math.abs(num1 - num2) <= 0.01) {
+            System.out.println("These numbers are the same.");
+        } else {
+            System.out.println("These numbers are different.");
+        }
+    }
+}
